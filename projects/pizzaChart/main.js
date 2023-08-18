7c29b3fe9d8d28dd460b9dea73399ac908601168
Facu Garay <5675299@mec.edu.py>
2023-08-17 21:37:32 -0400

    Create menu items on load with js

---
+++ projects/pizzaChart/main.js
@@ -0,0 +1,48 @@
+let domContainer =  document.querySelector(".pizzas"); 
+class MenuItem{
+  constructor(name = "Pizza", description = "Medium | mozzarella ", price = 12, img = "assets/pizza.png"){
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.img = img
+  }
+  create(){
+    let container = document.createElement("article");
+    let img = document.createElement("img");
+    let descriptionContainer = document.createElement("div");
+    let title = document.createElement("h2");
+    let description = document.createElement("p");
+    let price = document.createElement("p");
+    let button = document.createElement("button");
+    let buttonImg = document.createElement("img");
+    // Add style classes
+    container.classList.add("pizza");
+    img.classList.add("pizza--img");
+    descriptionContainer.classList.add("pizzas--description");
+    title.classList.add("description--title");
+    description.classList.add("description--text");
+    price.classList.add("description--price");
+    button.classList.add("description--add");
+    //Adding html content 
+    buttonImg.setAttribute("src", "assets/plus.png");
+    img.setAttribute("src", this.img);
+    descriptionContainer.innnerText = this.description;
+    title.innerText = this.name;
+    description.innerText = this.description;
+    price.innerText = "$"+this.price;
+    // Append elemnts
+    button.append(buttonImg, "ADD");
+    descriptionContainer.append(title, description, price, button);
+    container.append(img,descriptionContainer);
+    domContainer.appendChild(container);
+  }
+}
+const pizza1 = new MenuItem();
+const pizza2 = new MenuItem();
+const pizza3 = new MenuItem();
+
+window.addEventListener("load", ()=>{
+  pizza1.create();
+  pizza2.create();
+  pizza3.create();
+})
